689983dae154c8dda720f6a3a795859ef5b9069d
Sahar Maisha <saharmaisha@gmail.com>
2023-12-22 21:29:17 -0500

    added footer and routes

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,8 +1,11 @@
 import type { MetaFunction } from "@remix-run/node";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTwitter, faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons';
 import NavBar from "./nav";
 import Education from "./education"
 import Experience from "./experience"
 import Skills from "./skills"
+import Projects from './projects';
 import FadeIn from './fadein';
 
 
@@ -17,47 +20,65 @@ export default function Index() {
       <NavBar />
       <FadeIn>
       <div className="flex justify-center items-center min-h-screen">
-        <section id="" className="max-w-7xl flex flex-col sm:flex-row px-4 sm:px-6 lg:px-8 mx-auto my-auto">
+        <section id="about" className="max-w-7xl flex flex-col sm:flex-row px-4 sm:px-6 lg:px-8 mx-auto my-auto pt-32">
           <div className="text-center sm:flex-1">
             <h1 className="text-[10vw] sm:text-[5vw] font-bold">Hi there! I'm Sahar 👋</h1>
-              <div className="text-lg lg:text-xl text-center">
-                <p className="mb-4">
-                  I'm currently a student at Harvard College studying computer science.
-                </p>
-                <p className="mb-4">
-                  I'm interested in artificial intelligence and entrepreneurship. Most recently, I interned at Amazon on their Amazon Advertising team.
-                </p>
-                <p className="mb-4">
-                In my free time, I enjoy cooking and exploring new restaurants in NYC and Boston. Feel free to reach out to talk about anything tech, entrepreneurship, or food at <a href="mailto:smaisha@college.harvard.edu" className="text-blue-500">smaisha@college.harvard.edu</a>.              </p>
-              </div>
+            <div className="text-lg lg:text-xl text-center">
+              <p className="mb-4">
+                I'm currently a student at Harvard College studying computer science.
+              </p>
+              <p className="mb-4">
+                I'm interested in artificial intelligence and entrepreneurship. Most recently, I interned at Amazon on their Amazon Advertising team.
+              </p>
+              <p className="mb-4">
+                In my free time, I enjoy cooking and exploring new restaurants in NYC and Boston. Feel free to reach out to talk about anything tech, entrepreneurship, or food at <a href="mailto:smaisha@college.harvard.edu" className="text-blue-500">smaisha@college.harvard.edu</a>.
+              </p>
+            </div>
           </div>
           <div className="lg:flex-1 flex justify-center items-center">
             <div className="w-[20vw] sm:w-[25vw] h-[20vw] sm:h-[25vw] min-w-[10rem] min-h-[10rem] rounded-full overflow-hidden">
-              <img
-                src="images/me.jpg"
-                alt="Sahar's image"
-                className="w-full h-full object-cover"
-              />
+              <img src="images/me.jpg" alt="Sahar's image" className="w-full h-full object-cover" />
             </div>
           </div>
         </section>
       </div>
       </FadeIn>
-      <section id="education" className="max-w-7xl flex flex-col sm:flex-row px-4 sm:px-6 lg:px-8 mx-auto pt-36 pb-16 sm:pb-16">
+      <section id="education" className="max-w-7xl flex flex-col sm:flex-row lg:px-8 mx-auto pt-32">
       <FadeIn>
         <Education />
         </FadeIn>
       </section>
-      <section id="experience" className="max-w-7xl flex flex-col sm:flex-row px-4 sm:px-6 lg:px-8 mx-auto pt-36 pb-16 sm:pb-16">
+      <section id="experience" className="max-w-7xl flex flex-col sm:flex-row lg:px-8 mx-auto pt-32">
         <FadeIn>
           <Experience />
         </FadeIn>
       </section>
-      <section id="skills" className="max-w-7xl flex flex-col sm:flex-row px-4 sm:px-6 lg:px-8 mx-auto my-auto">
+      <section id="skills" className="max-w-7xl flex flex-col sm:flex-row lg:px-8 mx-auto pt-32">
         <FadeIn>
           <Skills />
         </FadeIn>
       </section>
+      <section id="projects" className="max-w-7xl flex flex-col sm:flex-row lg:px-8 mx-auto pt-32">
+        <FadeIn>
+          <Projects />
+        </FadeIn>
+      </section>
+      <footer className="bg-gray-200 text-center p-6">
+      <div className="flex justify-center mb-6">
+        <a href="https://twitter.com/saharmaisha" target="_blank" rel="noopener noreferrer" className="mr-6 text-gray-600">
+          <img src="images/x.png" alt="X Logo" className="h-5 w-5" />
+        </a>
+        <a href="https://linkedin.com/in/sahar-maisha" target="_blank" rel="noopener noreferrer" className="mr-6 text-gray-600">
+          <FontAwesomeIcon icon={faLinkedin} />
+        </a>
+        <a href="https://github.com/saharmaisha" target="_blank" rel="noopener noreferrer" className="text-gray-600">
+          <FontAwesomeIcon icon={faGithub} />
+        </a>
+      </div>
+      <div className="text-gray-700 text-center">
+        © 2024 Sahar Maisha
+      </div>
+    </footer>
     </>
   );
 }
